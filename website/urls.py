4f8b8fea338834b625090b82eefe000ee7f2732a
aashutosh396 <aashutosh4945@gmail.com>
2023-01-08 11:16:25 +0545

    form and CR

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
     home,
+    products
 )
 
 urlpatterns = [
     path('', home ),
+    path('products', products, name="product")
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+from product.models import (
+    Product,
+)
 
 
 def home(request):
@@ -8,5 +12,42 @@ def home(request):
     context = {}
     context["username"] = "12C"
     context["last_name"] = "aaa"
+
+    return render(request, template_path, context)
+
+
+def create_products(product_data=None):
+    if product_data:
+        # name = product_data["name"]
+        # qty = product_data["quantity"]
+        # price = product_data["price"]
+
+        # approach 1
+        # Product.objects.create(name=name, quantity=qty, price=price)
+
+        # approach 2
+        data = product_data.copy().dict()
+        data.pop('csrfmiddlewaretoken')
+        Product.objects.create(**data)
+
+        # approach 3
+        # data = {
+        #     "quantity": 12,
+        #     "price": 34
+        # }
+        # Product.objects.create(name=name, **data)
+
+
+
+def products(request):
+    template_path = 'product/product.html'
+    context = {}
+
+    products = Product.objects.filter(price=12)
+    context["products"] = products
+
+    if request.method == "POST":
+        create_products(request.POST)
+        return redirect('product')
 
     return render(request, template_path, context)
